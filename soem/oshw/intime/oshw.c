91e5abaf1c736a4cfff56816e6d85760fef93c14
CyrusGP <cyrusgp@gmail.com>
2025-09-22 20:37:00 -0700

    Fix oshw_find_adapters  only returns compatible networks

--- soem/oshw/intime/oshw.c
+++ soem/oshw/intime/oshw.c
@@ -13,6 +13,9 @@
 #include <string.h>
 #include <errno.h>
 #include "oshw.h"
+#include "hpeif2.h"
+
+#define MAX_NIC_INSTANCE 4
 
 /**
  * Host to Network byte order (i.e. to big endian).
@@ -43,95 +46,69 @@ uint16 oshw_ntohs (uint16 network)
  */
 ec_adaptert * oshw_find_adapters (void)
 {
-   struct ifaddrs *interfaces = NULL;
-   struct ifaddrs *ifa = NULL;
    ec_adaptert * adapter;
    ec_adaptert * prev_adapter = NULL;
    ec_adaptert * ret_adapter = NULL;
    int i = 0;
 
-   /* Get network interface addresses using INtime getifaddrs API */
-   if (getifaddrs(&interfaces) != 0)
+   /* List of supported HPE interface names */
+   const char* supported_interfaces[] = {"ie1g", "rtl1g", "rtl1gl"};
+   int num_supported = sizeof(supported_interfaces) / sizeof(supported_interfaces[0]);
+
+   /* Probe each supported interface type up to MAX_NIC_INSTANCE */
+   for (int interface_idx = 0; interface_idx < num_supported; interface_idx++)
    {
-      /* Error getting interface addresses */
-      return NULL;
+      for (int instance = 0; instance < MAX_NIC_INSTANCE; instance++)
+      {
+         char ifname[64];
+         HPEHANDLE handle;
+         HPESTATUS status;
+
+         /* Create interface name with instance number */
+         snprintf(ifname, sizeof(ifname), "%s%d", supported_interfaces[interface_idx], instance);
+
+         /* Try to open the HPE interface with PROBE_ONLY to check if it exists */
+         status = hpeOpen(ifname, SPEED_1000 | DUPLEX_FULL | PROBE_ONLY, NO_INTERRUPT, &handle);
+
+         if (status == E_OK)
+         {
+            /* Close the handle immediately since we're only probing */
+            hpeClose(handle);
+
+            /* Allocate memory for adapter structure */
+            adapter = (ec_adaptert *)malloc(sizeof(ec_adaptert));
+            if (adapter == NULL)
+            {
+               /* Return what we have so far */
+               return ret_adapter;
+            }
+
+            /* Link the adapter to the list */
+            if (i)
+            {
+               prev_adapter->next = adapter;
+            }
+            else
+            {
+               ret_adapter = adapter;
+            }
+
+            /* Initialize the adapter structure */
+            adapter->next = NULL;
+
+            /* Copy interface name */
+            strncpy(adapter->name, ifname, EC_MAXLEN_ADAPTERNAME);
+            adapter->name[EC_MAXLEN_ADAPTERNAME-1] = '\0';
+
+            /* Create description */
+            snprintf(adapter->desc, EC_MAXLEN_ADAPTERNAME, "%s", ifname);
+            adapter->desc[EC_MAXLEN_ADAPTERNAME-1] = '\0';
+
+            prev_adapter = adapter;
+            i++;
+         }
+      }
    }
-
-   /* Iterate through all interfaces */
-   for (ifa = interfaces; ifa != NULL; ifa = ifa->ifa_next)
-   {
-      /* Skip interfaces that are down or don't have an address */
-      if (!(ifa->ifa_flags & IFF_UP) || ifa->ifa_addr == NULL)
-      {
-         continue;
-      }
-
-      /* Only include Ethernet interfaces (exclude loopback) */
-      if (ifa->ifa_flags & IFF_LOOPBACK)
-      {
-         continue;
-      }
-
-      /* Only include IPv4 interfaces to avoid duplicates */
-      if (ifa->ifa_addr->sa_family != AF_INET)
-      {
-         continue;
-      }
-
-      /* Skip virtual network interfaces that start with "ven" */
-      if (ifa->ifa_name && strncmp(ifa->ifa_name, "ven", 3) == 0)
-      {
-         continue;
-      }
-
-      /* Allocate memory for adapter structure */
-      adapter = (ec_adaptert *)malloc(sizeof(ec_adaptert));
-      if (adapter == NULL)
-      {
-         /* Clean up and return what we have so far */
-         if (interfaces)
-            freeifaddrs(interfaces);
-         return ret_adapter;
-      }
-
-      /* If we got more than one adapter save link list pointer to previous
-       * adapter. Else save as pointer to return.
-       */
-      if (i)
-      {
-         prev_adapter->next = adapter;
-      }
-      else
-      {
-         ret_adapter = adapter;
-      }
-
-      /* Initialize the adapter structure */
-      adapter->next = NULL;
-
-      /* Copy interface name */
-      if (ifa->ifa_name)
-      {
-         strncpy(adapter->name, ifa->ifa_name, EC_MAXLEN_ADAPTERNAME);
-         adapter->name[EC_MAXLEN_ADAPTERNAME-1] = '\0';
-      }
-      else
-      {
-         adapter->name[0] = '\0';
-      }
-
-      /* Create description - use interface name and flags information */
-      snprintf(adapter->desc, EC_MAXLEN_ADAPTERNAME, "INtime Network Interface (%s)",
-               ifa->ifa_name ? ifa->ifa_name : "unknown");
-      adapter->desc[EC_MAXLEN_ADAPTERNAME-1] = '\0';
-
-      prev_adapter = adapter;
-      i++;
-   }
-
-   /* Free the interfaces structure */
-   if (interfaces)
-      freeifaddrs(interfaces);
 
    return ret_adapter;
 }
